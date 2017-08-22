55cebb1e99dd8dc9e68bcb3c74cc31eb63a0e457
Koushik Dutta <koushd@gmail.com>
2017-08-22 23:10:40 +0200

    Change how we search for custom makefiles
    Change-Id: I00fdb8f425f675bff23924620bedb7d1c6a1b987

--- core/product_config.mk
+++ core/product_config.mk
@@ -181,8 +181,7 @@ all_product_configs := $(call get-product-makefiles,\
     $(SRC_TARGET_DIR)/product/AndroidProducts.mk)
 else
  ifneq ($(CUSTOM_BUILD),)
-    $(call import-products, device/why/$(CUSTOM_BUILD)/device.mk)
-    all_product_configs := device/why/$(CUSTOM_BUILD)/device.mk
+    all_product_configs := device/*/$(CUSTOM_BUILD)/why.mk
   else
     # Read in all of the product definitions specified by the AndroidProducts.mk
     # files in the tree.
